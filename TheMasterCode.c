358e28de5662e67e8f741eea747ee3e2ba36afea
takshiljain9-shah <Takshiljain.shah@gmail.com>
2026-01-20 13:59:30 +0530

    Update TheMasterCode.c

--- TheMasterCode.c
+++ TheMasterCode.c
@@ -8,3 +8,17 @@ typedef struct{
     char title[50];
     int complexity_score;
 }TechTask;
+
+// Pointers & DMA: Managing memory on the heap
+typedef struct{
+    Tech task *tasks;
+    int count;
+    int capacity;
+}TaskBuffer;
+
+//Initialize the dynamic buffer
+void init_buffer(TaskBuffer *tb){
+    tb->count = 0;
+    tb->capacity = 2;
+    tb->tasks = (TechTask *)malloc*(tb->capacity * sizeof(TechTask));
+}
